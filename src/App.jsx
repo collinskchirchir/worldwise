d7cd5e7c7618d56e3d626f293284a978e704a14b
Collins Chirchir <collinskchirchir@gmail.com>
2023-12-31 22:35:25 +0300

    main pages and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Homepage from './pages/Homepage.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+
 export default function App() {
-    return (
-        <div>WorldWise</div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 export default function Product() {
     return (
-        <div></div>
+        <div>Product</div>
     );
 }
