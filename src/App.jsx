6b271cb3f2fe86da52f859167369de94e8000283
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-01 17:57:21 +0300

    building pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from './pages/Pricing.jsx';
 import Homepage from './pages/Homepage.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import AppLayout from './pages/AppLayout.jsx';
+import Login from './pages/Login.jsx';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-import PageNav from '../components/PageNav.jsx';
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Page Not Found</h1>
-    </div>);
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
